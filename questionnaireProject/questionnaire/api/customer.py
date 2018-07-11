40d93636f36e4059a3f322b9476bd75a53b25bf5
lajos <1287174078@qq.com>
2018-07-11 08:35:24 +0800

    客户添加、修改、删除问题，客户发布问卷、提交审核问卷

--- questionnaireProject/questionnaire/api/customer.py
+++ questionnaireProject/questionnaire/api/customer.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-from api.utils import *
 from question.models import *
 from api.decorators import *
 from api.rest import Rest
@@ -13,10 +12,8 @@ class CustomerQuestionnaireRest(Rest):
         id = data.get('id', None)
         page = abs(int(data.get('page', 1)))
         limit = abs(int(data.get('limit', 10)))
-        state = data.get('state', 0)
         with_detail = data.get('with_detail', False)
         questionnaires = Questionnaire.objects.filter(customer=request.user.customer).all()
-        data = dict()
         if id:
             questionnaire_objs = questionnaires[id]
         else:
@@ -105,3 +102,131 @@ class CustomerQuestionnaireRest(Rest):
             return json_response({'delete_ids': ids})
         else:
             return json_response({'msg': '未选择要删除的问卷'})
+
+class CustomerQuestionRest(Rest):
+    @customer_required
+    def put(self, request, *args, **kwargs):
+        data = request.PUT
+        questionnaire_id = data.get('questionnaire_id', 0)
+        questionnaire_exists = Questionnaire.objects.filter(id=questionnaire_id,customer=request.user.customer, state__in=[0, 2, 3])
+        if questionnaire_exists:
+            questionnaire = questionnaire_exists[0]
+        else:
+            return params_error({
+                'questionnaire_id': '找不到问卷,或者问卷不可修改'
+            })
+        # 添加问题
+        question = Question()
+        question.questionnaire = questionnaire
+        question.title = data.get('title', '题纲')
+        question.category = data.get('category', 'radio')
+        question.index = int(data.get('index', 0))
+        question.save()
+        # 修改问卷状态
+        questionnaire.state = 0
+        questionnaire.save()
+        # 添加问题选项
+        # items=['aaaa','bbbb','cccc','ddddd']
+        items = data.get('items', [])
+
+        for item in items:
+            question_item = QuestionItem()
+            question_item.question = question
+            question_item.content = item
+            question_item.save()
+
+        return json_response({
+            'id': question.id
+        })
+
+    @customer_required
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        question_id = data.get('id', 0)
+        # 判断需要修改的问题是否存在
+        question_exits = Question.objects.filter(id=question_id, questionnaire__state__in=[
+            0, 2, 3], questionnaire__customer=request.user.customer)
+        if not question_exits:
+            return params_error({
+                'id': "该问题找不到,或者该问题所在问卷无法修改"
+            })
+        # 更新问题的属性
+        question = question_exits[0]
+        question.title = data.get('title', '题纲')
+        question.category = data.get('category', 'radio')
+        question.index = int(data.get('index', 0))
+        question.save()
+        # 更新问题所在问卷的状态
+        questionnaire = question.questionnaire
+        questionnaire.state = 0
+        questionnaire.save()
+
+        items = data.get('items', [])
+        question.questionitem_set.all().delete()
+        for item in items:
+            question_item = QuestionItem()
+            question_item.question = question
+            question_item.content = item
+            question_item.save()
+
+        return json_response({
+            'msg': '更新成功'
+        })
+
+    @customer_required
+    def delete(self, request, *args, **kwargs):
+        data = request.DELETE
+        ids = data.get('ids', [])
+        objs = Question.objects.filter(id__in=ids, questionnaire__state__in=[
+            0, 2, 3], questionnaire__customer=request.user.customer)
+        deleted_ids = [obj.id for obj in objs]
+        questionnaire_set = set()
+        for obj in objs:
+            questionnaire_set.add(obj.questionnaire)
+
+        for questionnaire in questionnaire_set:
+            questionnaire.state = 0
+            questionnaire.save()
+        objs.delete()
+        return json_response({
+            'deleted_ids': deleted_ids
+        })
+
+
+class CustomerQuestionnaireStateRest(Rest):
+    @customer_required
+    def put(self, request, *args, **kwargs):
+        data = request.PUT
+        questionnaire_id = data.get('questionnaire_id', 0)
+        state = data.get('questionnaire_state', False)
+        if state == 0:
+            questionnaire_exits = Questionnaire.objects.filter(
+                id=questionnaire_id, customer=request.user.customer, state=0)
+            if not questionnaire_exits:
+                return params_error({
+                    'questionnaire_id': '问卷找不到'
+                })
+            questionnaire = questionnaire_exits[0]
+            questionnaire.state = 4
+            questionnaire.save()
+            return json_response({
+                'state': "发布成功"
+            })
+        if state == 3:
+            questionnaire_exits = Questionnaire.objects.filter(
+                id=questionnaire_id, customer=request.user.customer, state=3)
+            if not questionnaire_exits:
+                return params_error({
+                    'questionnaire_id': '问卷找不到'
+                })
+            questionnaire = questionnaire_exits[0]
+            questionnaire.state = 4
+            questionnaire.save()
+            return json_response({
+                'state': "发布成功"
+            })
+        return params_error({
+            'questionnaire_id': '问卷找不到或当前状态不可修改'
+        })
+
+
